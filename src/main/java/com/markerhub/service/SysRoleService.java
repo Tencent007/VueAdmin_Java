31be3f1beb05d3f53446c51fd0c5cb0c7b29e5aa
Liu <yiwei.liu@accenture.com>
2021-11-12 11:49:25 +0800

    java

---
+++ src/main/java/com/markerhub/service/SysRoleService.java
@@ -0,0 +1,19 @@
+package com.markerhub.service;
+
+import com.markerhub.entity.SysRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 我的公众号：MarkerHub
+ * @since 2021-10-22
+ */
+public interface SysRoleService extends IService<SysRole> {
+
+    List<SysRole> listRolesByUserId(Long userId);
+}
